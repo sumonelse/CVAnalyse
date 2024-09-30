97beb8c9bf7685b4c6ddb320390d45412a8571ed
sumonelse <sumonelse01@gmail.com>
2024-09-30 05:59:40 +0530

    Create Logic of Upload

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,11 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+    content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+    theme: {
+        extend: {
+            boxShadow: {
+                primary:
+                    "0 4px 6px rgba(14, 165, 233, 0.3), 0 2px 4px rgba(14, 165, 233, 0.2), 0 1px 3px rgba(14, 165, 233, 0.1)",
+            },
+        },
+    },
+    plugins: [],
 }
