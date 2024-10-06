7d246ff333bbe61697d0c8a45ae74fe6a124593c
sumonelse <sumonelse01@gmail.com>
2024-10-06 21:44:17 +0530

    Create Company Model

---
+++ backend/src/company/companyModel.js
@@ -0,0 +1,44 @@
+import mongoose, { mongo } from "mongoose"
+
+const companySchema = new mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required: true,
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+        },
+        password: {
+            type: String,
+            required: true,
+        },
+        phoneNumber: {
+            type: String,
+            required: true,
+        },
+        location: {
+            type: String,
+            required: true,
+        },
+        jobsPosted: {
+            type: [
+                {
+                    type: mongoose.Schema.Types.ObjectId,
+                    ref: "Job",
+                },
+            ],
+        },
+        dateJoined: {
+            type: Date,
+            default: Date.now,
+        },
+    },
+    { versionKey: false }
+)
+
+const companyModel = mongoose.model("company", companySchema)
+
+export default companyModel
