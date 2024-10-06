2a051280361397f593657ae05226ba467eaa1a0e
sumonelse <sumonelse01@gmail.com>
2024-10-06 21:38:13 +0530

    Create Job Model

---
+++ backend/src/job/jobModel.js
@@ -0,0 +1,56 @@
+import mongoose from "mongoose"
+
+const jobSchema = new mongoose.Schema(
+    {
+        title: {
+            type: String,
+            required: true,
+        },
+        description: {
+            type: String,
+            required: true,
+        },
+        requirements: {
+            type: [String],
+            required: true,
+        },
+        salary: {
+            min: {
+                type: Number,
+                required: true,
+            },
+            max: {
+                type: Number,
+                required: true,
+            },
+        },
+        location: {
+            type: String,
+            required: true,
+        },
+        postedBy: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Company",
+            required: true,
+        },
+        applicants: [
+            {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: "Candidate", // assuming 'User' is your job applicant entity
+            },
+        ],
+        postedAt: {
+            type: Date,
+            default: Date.now,
+        },
+        deadline: {
+            type: Date,
+            required: true,
+        },
+    },
+    { versionKey: false }
+)
+
+const jobModel = mongoose.model("job", jobSchema)
+
+export default jobModel
