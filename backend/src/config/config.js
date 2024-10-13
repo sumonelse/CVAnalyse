f51bf2108c4e253d24168faa1706f681fe87545f
sumonelse <sumonelse01@gmail.com>
2024-10-13 07:51:23 +0530

    Update config

--- backend/src/config/config.js
+++ backend/src/config/config.js
@@ -5,6 +5,7 @@ const _config = {
     port: process.env.PORT,
     dbUrl: process.env.MONGO_CONNECTION_URL,
     env: process.env.NODE_ENV,
+    frontendDomain: process.env.FRONTEND_DOMAIN,
 }
 
 export const config = Object.freeze(_config)
