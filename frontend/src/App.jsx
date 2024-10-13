90fbc58b9eeb21ca779ebceaa021686bc847c09d
sumonelse <sumonelse01@gmail.com>
2024-10-13 07:54:53 +0530

    Attach Skills route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,12 +2,32 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom"
 import Home from "./pages/Home"
 import CompanyRegister from "./components/CompanyRegister"
 import CompanyLogin from "./components/CompanyLogin"
+import Skills from "./pages/Skills"
+import Jobs from "./pages/Jobs"
+import JobCreate from "./components/JobCreate"
 
 const App = () => {
     const router = createBrowserRouter([
         {
             path: "/",
             element: <Home />,
+        },
+        {
+            path: "/skills",
+            element: <Skills />,
+        },
+        {
+            path: "/jobs",
+            children: [
+                {
+                    path: "",
+                    element: <Jobs />,
+                },
+                {
+                    path: "create",
+                    element: <JobCreate />,
+                },
+            ],
         },
         {
             path: "/company",
